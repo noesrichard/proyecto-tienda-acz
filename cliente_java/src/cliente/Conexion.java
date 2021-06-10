cd0e9b9f3d6f88beabdbca40898cbe910b5089a0
Richard Carrion <rcarrion3406@gmail.com>
2021-06-10 12:48:20 -0500

    Nuevo proyecto java"

--- cliente_java/src/cliente/Conexion.java
+++ cliente_java/src/cliente/Conexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cliente_java;
+package cliente;
 import com.google.gson.Gson;
 import java.net.URI; 
 import java.net.http.HttpClient; 
