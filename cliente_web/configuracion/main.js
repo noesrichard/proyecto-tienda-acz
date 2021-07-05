3ee4f2ff7a569a30c6c2da95978f7d4d9d76449d
Richard Carrion <rcarrion3406@gmail.com>
2021-07-05 11:59:18 -0500

    Botones paypal agregados

--- cliente_web/configuracion/main.js
+++ cliente_web/configuracion/main.js
@@ -39,23 +39,23 @@ fetch(url)
         }
     })
 
-function searchProduct(category="", brand=""){ 
-    url = "http://proyecto-tienda-acz.herokuapp.com/catalog/products/search?"; 
-    if (category != ""){ 
-        url += ("category_id="+category+"&"); 
+function searchProduct(category = "", brand = "") {
+    url = "http://proyecto-tienda-acz.herokuapp.com/catalog/products/search?";
+    if (category != "") {
+        url += ("category_id=" + category + "&");
     }
-    if (brand != ""){ 
-        url += ("brand_id="+brand); 
+    if (brand != "") {
+        url += ("brand_id=" + brand);
     }
     console.log(url)
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        ol.innerHTML = ``;
-        console.log(data)
-        for (let i = 0; i < data.length; i++) {
-            ol.innerHTML +=
-                `
+        .then(res => res.json())
+        .then(data => {
+            ol.innerHTML = ``;
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                ol.innerHTML +=
+                    `
                     <!--Lista de productos-->
                     <li class="product-li">
                         <div id="product-img-div">
@@ -71,22 +71,22 @@ function searchProduct(category="", brand=""){
                         </div>
                     </li>
                 `
-        }
-    })
+            }
+        })
 }
 
-function productDescription(productId){ 
-    const root = document.getElementById('root'); 
-    const url = "https://proyecto-tienda-acz.herokuapp.com/catalog/products/"+productId+"/description"; 
-    var quantity = 5; 
-    root.innerHTML = ""; 
+function productDescription(productId) {
+    const root = document.getElementById('root');
+    const url = "https://proyecto-tienda-acz.herokuapp.com/catalog/products/" + productId + "/description";
+    var quantity = 5;
+    root.innerHTML = "";
     fetch(url)
-    .then(res => res.json())
-    .then(data => { 
-        console.log(data); 
-        console.log(quantity);
-        console.log(data.product.product_id);
-        root.innerHTML = `
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            console.log(quantity);
+            console.log(data.product.product_id);
+            root.innerHTML = `
             <div>
                 <div id="product-img-div">
                     <!--Aqui va la imagen-->
@@ -100,112 +100,124 @@ function productDescription(productId){
             </div>  
             <h2>Comentarios</h2>
         `
-        for (let i = 0; i<data.comments.length; i++){ 
-            root.innerHTML += `
+            for (let i = 0; i < data.comments.length; i++) {
+                root.innerHTML += `
                 <div id='comentario-${i}' class='comentarios'> 
                     <h3>${data.comments[i].qualification}</h3>
                     <p>${data.comments[i].description}</p>
                 </div>
             `
-            if (data.comments[i].username == "rmcv"){ 
-                document.getElementById("comentario-"+i).innerHTML += `
+                if (data.comments[i].username == "rmcv") {
+                    document.getElementById("comentario-" + i).innerHTML += `
                     <input type='button' value='Eliminar Comentario'> 
                 `
+                }
             }
-        }
-    })
+        })
 }
 
-function agregarAlCarrito(productId, quantity){ 
-    console.log("entra"); 
-    const url = "https://proyecto-tienda-acz.herokuapp.com/cart/products"; 
-    const username = "rmcv"; 
-    const password = "123"; 
-    const auth = btoa(username+":"+password); 
-    fetch(url, { 
-        'method': 'POST', 
-        'headers': { 
-            'Content-Type': 'application/json', 
-            'Authorization': 'Basic '+auth
+function agregarAlCarrito(productId, quantity) {
+    console.log("entra");
+    const url = "https://proyecto-tienda-acz.herokuapp.com/cart/products";
+    const username = "rmcv";
+    const password = "123";
+    const auth = btoa(username + ":" + password);
+    fetch(url, {
+        'method': 'POST',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + auth
         },
-        'body': JSON.stringify({ 
-            'product': productId, 
+        'body': JSON.stringify({
+            'product': productId,
             'quantity': quantity
         })
     })
-    .then(res => { 
-        console.log(res.status); 
-        if (res.status == 200 && document.getElementById('carrito-div').innerHTML != ""){ 
-            cargarCarrito(document.getElementById('carrito-div'))
-        }
-    }); 
+        .then(res => {
+            console.log(res.status);
+            if (res.status == 200 && document.getElementById('carrito-div').innerHTML != "") {
+                cargarCarrito(document.getElementById('carrito-div'))
+            }
+        });
 }
-const btnCarrito = document.getElementById("btn-carrito"); 
-btnCarrito.addEventListener('click',abrirCarrito); 
-function cargarCarrito(divCarrito){ 
-    divCarrito.style.textAlign = "center"; 
-    divCarrito.innerHTML = "<h1> Carrito </h1>"; 
-    const url = "https://proyecto-tienda-acz.herokuapp.com/cart/products"; 
-    const username = "rmcv"; 
-    const password = "123"; 
-    const auth = btoa(username+":"+password); 
-    fetch(url, { 
-        'method': 'GET', 
-        'headers': { 
-            'Content-Type': 'application/json', 
-            'Authorization': 'Basic '+auth
+const btnCarrito = document.getElementById("btn-carrito");
+btnCarrito.addEventListener('click', abrirCarrito);
+function cargarCarrito(divCarrito) {
+    divCarrito.style.textAlign = "center";
+    divCarrito.innerHTML = "<h1> Carrito </h1>";
+    const url = "https://proyecto-tienda-acz.herokuapp.com/cart/products";
+    const username = "rmcv";
+    const password = "123";
+    const auth = btoa(username + ":" + password);
+    fetch(url, {
+        'method': 'GET',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + auth
         }
     })
-    .then(res => res.json())
-    .then(data => { 
-        console.log(data)
-        var total = 0; 
-        for (let i = 0; i<data.length ; i++){ 
-            var precioProductoTotal = data[i].product_price*data[i].product_quantity;
-            divCarrito.innerHTML += `
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            var total = 0;
+            for (let i = 0; i < data.length; i++) {
+                var precioProductoTotal = data[i].product_price * data[i].product_quantity;
+                divCarrito.innerHTML += `
                 <label>${data[i].product_name} </label><br> 
                 <label>Precio Unitario: ${data[i].product_price}</label><br>    
                 <label>Cantidad: ${data[i].product_quantity}</label><br>    
-                <label>Precio Total: ${data[i].product_price*data[i].product_quantity}</label><br>    
+                <label>Precio Total: ${data[i].product_price * data[i].product_quantity}</label><br>    
                 <input type='button' value='Eliminar del Carrito' onclick=eliminarDelCarrito(${data[i].cart_id})><br>
             `
-            total += data[i].product_price*data[i].product_quantity;  
-        }
-        divCarrito.innerHTML += `
+                total += data[i].product_price * data[i].product_quantity;
+            }
+            divCarrito.innerHTML += `
             <br><label>Total a pagar: ${total}</label><br>
-            <input type='button' value='Comprar' class='blue-btn'>
+            <div id="paypal-button-container"></div>
         `;
-    })
+            paypal.Buttons({
+                createOrder: function (data, actions) {
+                    // This function sets up the details of the transaction, including the amount and line item details.
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: total 
+                            }
+                        }]
+                    });
+                }
+            }).render('#paypal-button-container')
+        })
 }
-function abrirCarrito(){ 
-    const divCarrito = document.getElementById("carrito-div"); 
-    if ( divCarrito.innerHTML == ""){ 
-        cargarCarrito(divCarrito);         
-    }else{ 
-        divCarrito.innerHTML = ""; 
+function abrirCarrito() {
+    const divCarrito = document.getElementById("carrito-div");
+    if (divCarrito.innerHTML == "") {
+        cargarCarrito(divCarrito);
+    } else {
+        divCarrito.innerHTML = "";
     }
 
 }
 
-function eliminarDelCarrito(carritoId){ 
-    var url = 'https://proyecto-tienda-acz.herokuapp.com/cart/products/'+carritoId; 
-    const username = "rmcv"; 
-    const password = "123"; 
-    const auth = btoa(username+":"+password); 
-    fetch(url, { 
-        'method': 'DELETE', 
-        'headers': { 
-                'Content-Type': 'application/json', 
-                'Authorization': 'Basic '+auth
-            }       
-    }) 
-    .then(res => res.json())
-    .then(data => { 
-        if (data.message == "Todo bien"){ 
-            cargarCarrito(document.getElementById('carrito-div'))
-        }
-        else{ 
-            console.log('No se pudo borrar el producto!')
+function eliminarDelCarrito(carritoId) {
+    var url = 'https://proyecto-tienda-acz.herokuapp.com/cart/products/' + carritoId;
+    const username = "rmcv";
+    const password = "123";
+    const auth = btoa(username + ":" + password);
+    fetch(url, {
+        'method': 'DELETE',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + auth
         }
     })
+        .then(res => res.json())
+        .then(data => {
+            if (data.message == "Todo bien") {
+                cargarCarrito(document.getElementById('carrito-div'))
+            }
+            else {
+                console.log('No se pudo borrar el producto!')
+            }
+        })
 }
