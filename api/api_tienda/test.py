91e4703a9c3ec73cf79aa4befffaaf715839bd94
CarrionValarezo <84354741+CarrionValarezo@users.noreply.github.com>
2021-07-10 14:10:17 -0500

    Agregando test (#29)

---
+++ api/api_tienda/test.py
@@ -0,0 +1,29 @@
+import unittest
+from users.models import User
+from users import validator
+
+
+class Test(unittest.TestCase):
+
+    def test_valid_username(self):
+        user = User(username="ric.-r")
+        result = validator.validate_username(user)
+        self.assertTrue(result)
+
+    def test_valid_password(self):
+        user = User(password="12qwe.reu_12")
+        result = validator.validate_password(user)
+        self.assertTrue(result)
+
+    def test_invalid_username(self):
+        user = User(username="ric .-r")
+        result = validator.validate_username(user)
+        self.assertFalse(result)
+
+    def test_invalid_password(self):
+        user = User(password="12qwe .reu_12")
+        result = validator.validate_password(user)
+        self.assertFalse(result)
+
+if __name__ == "__main__":
+    unittest.main()
